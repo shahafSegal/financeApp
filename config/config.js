06915d837d52d73b9faa2cc6f9c91382e4d6c1bc
shahafSegal <goigadol360@gmail.com>
2024-01-22 16:00:38 +0200

    added firebase

--- config/config.js
+++ config/config.js
@@ -7,3 +7,21 @@ headers: {
 };
   
 export {requestOptions}
+
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD_m0GZxtA7Nf05_OeFKC6_UMsfwXkDqPo",
+  authDomain: "coinbase-1ac10.firebaseapp.com",
+  projectId: "coinbase-1ac10",
+  storageBucket: "coinbase-1ac10.appspot.com",
+  messagingSenderId: "204614415031",
+  appId: "1:204614415031:web:b64f7f48546c779a2a1332"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export{app}
